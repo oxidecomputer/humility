05b0b018371a2371afa2cc32206373a6c035c3e1
Bryan Cantrill <bryan@oxide.computer>
2021-09-20 00:08:01 -0700

    "humility rencm": add the ability to write to registers

--- src/cmd/rencm.rs
+++ src/cmd/rencm.rs
@@ -53,7 +53,10 @@ struct RencmArgs {
 
     /// specifies register(s) to read
     #[structopt(
-        long, short, value_name = "register", conflicts_with = "module"
+        long,
+        short,
+        value_name = "register",
+        conflicts_with = "module"
     )]
     register: Option<Vec<String>>,
 
@@ -74,16 +77,18 @@ fn rencm(
     _args: &Args,
     subargs: &Vec<String>,
 ) -> Result<()> {
+    use idt8a3xxxx::*;
+
     let subargs = RencmArgs::from_iter_safe(subargs)?;
 
-    let modules = idt8a3xxxx::modules();
+    let modules = modules();
     let mut context = HiffyContext::new(hubris, core, subargs.timeout)?;
     let funcs = context.functions()?;
-    let func = funcs
+    let read_func = funcs
         .get("I2cRead")
         .ok_or_else(|| anyhow!("did not find I2cRead function"))?;
 
-    if func.args.len() != 7 {
+    if read_func.args.len() != 7 {
         bail!("mismatched function signature on I2cRead");
     }
 
@@ -99,7 +104,7 @@ fn rencm(
 
     if let Some(ref portarg) = subargs.port {
         let p = hubris
-            .lookup_enum(func.args[1])
+            .lookup_enum(read_func.args[1])
             .context("expected port to be an enum")?;
 
         if p.size != 1 {
@@ -156,8 +161,8 @@ fn rencm(
         if module.base.len() > 1 {
             for i in 0..module.base.len() {
                 modnames.insert(
-                    format!("{}_{}", module.name, i), 
-                    (module, Some(i))
+                    format!("{}_{}", module.name, i),
+                    (module, Some(i)),
                 );
             }
         }
@@ -166,7 +171,7 @@ fn rencm(
 
         for register in module.registers {
             match regmods.insert(register.name, (module, None)) {
-                None => {},
+                None => {}
                 Some(_) => {
                     bail!("duplicate register {}!", register.name);
                 }
@@ -178,7 +183,23 @@ fn rencm(
 
     if let Some(registers) = &subargs.register {
         for register in registers {
-            let s = register.split(".").collect::<Vec<&str>>();
+            let val = register.split("=").collect::<Vec<&str>>();
+
+            let write = if val.len() > 1 {
+                if val.len() != 2 {
+                    bail!("illegal register set value");
+                }
+
+                if let Ok(val) = parse_int::parse::<u64>(val[1]) {
+                    Some(val)
+                } else {
+                    bail!("invalid register value {}", val[1]);
+                }
+            } else {
+                None
+            };
+
+            let s = val[0].split(".").collect::<Vec<&str>>();
 
             let (module, register) = if s.len() == 2 {
                 //
@@ -189,7 +210,7 @@ fn rencm(
                     None => {
                         bail!("{} is not a valid module name", s[0]);
                     }
-                    Some(module) => (module, s[1])
+                    Some(module) => (module, s[1]),
                 }
             } else if s.len() == 1 {
                 //
@@ -200,7 +221,7 @@ fn rencm(
                     None => {
                         bail!("{} is not a known register", s[0]);
                     }
-                    Some(module) => (module, s[0])
+                    Some(module) => (module, s[0]),
                 }
             } else {
                 bail!("expected module.register");
@@ -211,18 +232,16 @@ fn rencm(
             // enqueue the work to do.
             //
             match module.0.registers.iter().find(|r| r.name == register) {
-                Some(r) => {
-                    match module.1 {
-                        Some(ndx) => {
-                            work.push((module.0, r, ndx));
-                        }
-                        None => {
-                            for ndx in 0..module.0.base.len() {
-                                work.push((module.0, r, ndx));
-                            }
+                Some(r) => match module.1 {
+                    Some(ndx) => {
+                        work.push((module.0, r, ndx, write));
+                    }
+                    None => {
+                        for ndx in 0..module.0.base.len() {
+                            work.push((module.0, r, ndx, write));
                         }
                     }
-                }
+                },
                 None => {
                     let registers = module.0.registers;
 
@@ -243,22 +262,20 @@ fn rencm(
                 None => {
                     bail!("{} is not a valid module name", module);
                 }
-                Some(module) => {
-                    match module.1 {
-                        Some(ndx) => {
-                            for r in module.0.registers {
-                                work.push((module.0, r, ndx));
-                            }
+                Some(module) => match module.1 {
+                    Some(ndx) => {
+                        for r in module.0.registers {
+                            work.push((module.0, r, ndx, None));
                         }
-                        None => {
-                            for ndx in 0..module.0.base.len() {
-                                for r in module.0.registers {
-                                    work.push((module.0, r, ndx));
-                                }
+                    }
+                    None => {
+                        for ndx in 0..module.0.base.len() {
+                            for r in module.0.registers {
+                                work.push((module.0, r, ndx, None));
                             }
                         }
                     }
-                }
+                },
             }
         }
     }
@@ -267,13 +284,13 @@ fn rencm(
         for module in modules {
             for ndx in 0..module.base.len() {
                 for r in module.registers {
-                    work.push((module, r, ndx));
+                    work.push((module, r, ndx, None));
                 }
             }
         }
     }
 
-    info!("{} registers to read", work.len());
+    info!("{} registers found", work.len());
 
     let mut ndx = 0;
     let maxops = 1000;
@@ -282,10 +299,22 @@ fn rencm(
         bail!("must specify -s, -M, or -r");
     }
 
+    let jobname = |job: &(&Module, &Register, usize, Option<u64>)| {
+        if job.0.base.len() == 1 {
+            format!("{}.{}", job.0.name, job.1.name)
+        } else {
+            format!("{}_{}.{}", job.0.name, job.2, job.1.name)
+        }
+    };
+
+    let jobaddr = |job: &(&Module, &Register, usize, Option<u64>)| {
+        job.0.base[job.2] + job.1.offset
+    };
+
     loop {
         let mut ops = vec![];
         let mut current = None;
-        let mut rndx = ndx;
+        let mut calls = vec![];
 
         ops.push(Op::Push(subargs.controller));
 
@@ -313,7 +342,7 @@ fn rencm(
             match current {
                 Some(current) if current == page => {}
                 _ => {
-                    // 
+                    //
                     // If our page doesn't match our current page (or if we
                     // don't have a current page), we need to write our page
                     // address.
@@ -324,13 +353,42 @@ fn rencm(
                     ops.push(Op::Call(write_func.id));
                     ops.push(Op::DropN(3));
                     current = Some(page);
+                    calls.push(None);
                 }
             }
 
             ops.push(Op::Push(idt8a3xxxx::offset(addr)));
-            ops.push(Op::Push(job.1.contents.size()));
-            ops.push(Op::Call(func.id));
-            ops.push(Op::DropN(2));
+
+            if let Some(write) = job.3 {
+                let mut buf = vec![0; 16];
+                let size = job.1.contents.size();
+
+                let payload = match Payload::into_slice(
+                    job.1.contents,
+                    write,
+                    buf.as_mut_slice(),
+                ) {
+                    Some(payload) => payload,
+                    None => {
+                        let name = jobname(&job);
+                        bail!("value {} exceeds size for {}", write, name);
+                    }
+                };
+
+                for i in 0..payload.data.len() {
+                    ops.push(Op::Push(payload.data[i]));
+                }
+
+                ops.push(Op::Push(size));
+                ops.push(Op::Call(write_func.id));
+                ops.push(Op::DropN(2 + size));
+            } else {
+                ops.push(Op::Push(job.1.contents.size()));
+                ops.push(Op::Call(read_func.id));
+                ops.push(Op::DropN(2));
+            }
+
+            calls.push(Some(ndx));
             ndx += 1;
         }
 
@@ -351,15 +409,47 @@ fn rencm(
 
         let results = context.results(core)?;
 
-        for result in &results {
-            match result {
+        if results.len() != calls.len() {
+            bail!(
+                "short results; expected {}, found {}: {:?}",
+                calls.len(),
+                results.len(),
+                results
+            );
+        }
+
+        for rndx in 0..results.len() {
+            match results[rndx] {
                 Ok(ref r) => {
                     if r.len() == 0 {
+                        if let Some(ndx) = calls[rndx] {
+                            let job = work[ndx];
+
+                            if let Some(write) = job.3 {
+                                info!(
+                                    "successfully wrote {} to {} at 0x{:x}",
+                                    write,
+                                    jobname(&job),
+                                    jobaddr(&job),
+                                );
+                            } else {
+                                bail!(
+                                    "missing result at {}: {:?}",
+                                    rndx,
+                                    results
+                                );
+                            }
+                        }
+
                         continue;
                     }
 
-                    let job = work[rndx];
-                    let addr = job.0.base[job.2] + job.1.offset;
+                    let job = match calls[rndx] {
+                        Some(ndx) => work[ndx],
+                        None => {
+                            bail!("spurious result at {}: {:?}", rndx, results);
+                        }
+                    };
 
                     let payload = match idt8a3xxxx::Payload::from_slice(
                         job.1.contents,
@@ -367,28 +457,47 @@ fn rencm(
                     ) {
                         Some(payload) => payload,
                         None => {
-                            bail!("short read for {:?} at {:?}", job, result)
+                            bail!("short read for {:?} at {:?}", job, results)
                         }
                     };
 
                     let value = payload.value();
 
-                    let name = if job.0.base.len() == 1 {
-                        format!("{}.{}", job.0.name, job.1.name)
-                    } else {
-                        format!("{}_{}.{}", job.0.name, job.2, job.1.name)
-                    };
-
                     println!(
                         "0x{:04x} {:50} = 0x{:x} ({})",
-                        addr, name, value, value
+                        jobaddr(&job),
+                        jobname(&job),
+                        value,
+                        value
                     );
-                    rndx += 1;
                 }
-                Err(_) => {
-                    bail!("failed: {:?}", results);
+
+                Err(code) => {
+                    if let Some(ndx) = calls[rndx] {
+                        let job = work[ndx];
+
+                        let err = match job.3 {
+                            None => ("read", read_func.strerror(code)),
+                            Some(_) => ("write", write_func.strerror(code)),
+                        };
+
+                        bail!(
+                            "failed to {} {} at 0x{:x}: {}: {:?}",
+                            err.0,
+                            jobname(&job),
+                            jobaddr(&job),
+                            err.1,
+                            results
+                        );
+                    } else {
+                        bail!(
+                            "failed to page write: {}: {:?}",
+                            write_func.strerror(code),
+                            results,
+                        );
+                    }
                 }
-            }            
+            }
         }
 
         if ndx == work.len() {
