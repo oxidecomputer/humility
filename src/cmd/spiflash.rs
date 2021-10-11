32b684badcff6b8a364f7dd16b3d94e38eae9b26
Ben Stoltz <benstoltz@gmail.com>
2021-10-11 10:06:58 -0700

    remove spurious task param for spiflash

--- src/cmd/spiflash.rs
+++ src/cmd/spiflash.rs
@@ -11,15 +11,15 @@ use std::str;
 use std::thread;
 
 use anyhow::{anyhow, bail, Result};
+use core::num::ParseIntError;
 use hif::*;
+use std::path::PathBuf;
 use std::time::Duration;
 use structopt::clap::App;
 use structopt::StructOpt;
-use core::num::{ParseIntError};
-use std::path::PathBuf;
 // use std::io;
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 use std::io::SeekFrom;
 
 fn parse_hex(src: &str) -> Result<u32, ParseIntError> {
@@ -93,7 +93,7 @@ enum Command {
         address: u32,
 
         /// Data transfer length
-        /// 
+        ///
         /// Length can be inferred from input file or --data option
         #[structopt(parse(try_from_str = parse_hex))]
         length: Option<u32>,
@@ -107,7 +107,7 @@ enum Command {
         input: Option<PathBuf>,
 
         /// comma-separated hex bytes to write
-        /// 
+        ///
         /// Take list length as --nbytes or if --nbytes then truncate
         /// or pad with 0xFF.
         #[structopt(long, short)]
@@ -126,7 +126,7 @@ enum Command {
 
         /// Some instructions require an additional parameter
         param: Option<u32>,
-    }
+    },
 }
 
 fn spiflash(
@@ -152,9 +152,9 @@ fn spiflash(
         Ok(f)
     };
 
-    let spiflash_read = func("SpiFlashRead", 4)?;
-    let spiflash_write = func("SpiFlashWrite", 4)?;
-    let spiflash_get = func("SpiFlashGet", 4)?;
+    let spiflash_read = func("SpiFlashRead", 3)?;
+    let spiflash_write = func("SpiFlashWrite", 3)?;
+    let spiflash_get = func("SpiFlashGet", 3)?;
 
     let tasks = hubris.lookup_feature("spiflash");
     if !tasks.is_ok() {
@@ -169,7 +169,7 @@ fn spiflash(
 
     // All of the SpiFlash commands are in the form:
     //
-    //    (Task, Instruction, Option<32>, Option<32>, Buffer)
+    //    (Instruction, Option<32>, Option<32>, Buffer)
     //
     // Instruction is a SPI NOR Flash opcode from the MT25Q instruction set.
     // Those instructions are mapped 1:1 for now for the actual MT25Q device
@@ -178,154 +178,147 @@ fn spiflash(
     // set or another manufacturer's set if needed.
     let mut ops = vec![];
 
-    if let HubrisTask::Task(task) = task {
-        ops.push(Op::Push32(task));
-    } else {
-        bail!("SPIFLASH task cannot be the kernel");
-    }
-
-    info!("SPIFLASH master is {}", hubris.lookup_module(task)?.name);
-
     let timeout = subargs.timeout;
     let buffer = match subargs.cmd {
-        Command::Get{instruction, length, param} => {
-        // e.g. "spiflash get -I jedecid -l 3"
-        println!("inst {:?}", instruction);
-        println!("length {:?}", length);
-        println!("param {:?}", param);
-        ops.push(Op::Push(instruction as u8));
-        let arr = if length.is_some() {
-            let getlen = length.unwrap();
-            ops.push(Op::Push32(getlen));
-            let mut arr = vec![0u8; getlen as usize];
-            Some(arr)
-        } else {
-            ops.push(Op::PushNone);
-            None
-        };
-        if param.is_some() {
-            ops.push(Op::Push32(param.unwrap()));    // optional param for some instructions
-        } else {
-            ops.push(Op::PushNone);    // optional param for some instructions
+        Command::Get { instruction, length, param } => {
+            // e.g. "spiflash get -I jedecid -l 3"
+            println!("inst {:?}", instruction);
+            println!("length {:?}", length);
+            println!("param {:?}", param);
+            ops.push(Op::Push(instruction as u8));
+            let arr = if length.is_some() {
+                let getlen = length.unwrap();
+                ops.push(Op::Push32(getlen));
+                let mut arr = vec![0u8; getlen as usize];
+                Some(arr)
+            } else {
+                ops.push(Op::PushNone);
+                None
+            };
+            if param.is_some() {
+                ops.push(Op::Push32(param.unwrap())); // optional param for some instructions
+            } else {
+                ops.push(Op::PushNone); // optional param for some instructions
+            }
+            ops.push(Op::Call(spiflash_get.id));
+            arr
         }
-        ops.push(Op::Call(spiflash_get.id));
-        arr
-    },
-     Command::Read{ address, length, output } => {
-        // e.g. spiflash read 4000 100 --out ./data.bin
-        println!("address {:?}", address);
-        println!("length {:?}", length);
-        println!("output {:?}", output);
-        // XXX What is the right order to push these Ops?
-        ops.push(Op::Push(parse_instruction("read").unwrap() as u8));
-        ops.push(Op::Push32(address));
-        ops.push(Op::Push32(length));
-        let mut arr = vec![0u8; length as usize];
-        ops.push(Op::Call(spiflash_read.id));
-        Some(arr)
-    },
-     Command::Write{address, length, offset, input, data} => {
-        // Write data to a SPI flash.
-        //
-        // Hubris drv_spiflash_api handles issuing the WEN instruction before PP.
-        //
-        // e.g. spiflash write [--erase] address <length> \
-        //          [-offset xxx] [--input path] [--data xx,xx,... ]
-
-        // Note: It is assumed that a chip erase has been done and that no page
-        // needs to be erased before programming.
-        // Alternatively, a series of sector erase commands could be issued to
-        // reset bytes in the flash before programming an area.
-        // Some devices have limits on the safety of multiple writes to the same
-        // flash area (line, page, sector) between erases.
-        // TODO: check supported write/erase attributes of the parts being used.
-        println!("address {:?}", address);
-        // If length is not specified, it should be the length of the source.
-        // If length is specified, it should override the length of the source.
-        // If the source has less data than specified by length, that's an error.
-        println!("length {:?}", length);
-
-        // There are limits on buffer size that can be transferred to Hubris,
-        // so, an offset into the source is allowed.
-        // TODO: Add appropriate buffering and looping to allow fully automated
-        // writing of entire flash images.
-        println!("offset {:?}", offset);
-
-        println!("input {:?}", input);
-        println!("data {:?}", data);
-        // Input and Data should not be specified at the same time.
-        if input.is_some() == data.is_some() {
-            bail!("One of '--in path' or '--data bytes' are required, but not both");
-        }
-
-        let buffer = if data.is_some() {
-            // Parse a string of comma separated hex bytes.
-            let data = data.unwrap();
-            let bytes: Vec<&str> = data.split(",").collect();
-            let mut arr = Vec::new();
-
-            for byte in &bytes {
-                if let Ok(val) = u8::from_str_radix(byte, 16) {
-                    arr.push(val);
-                } else {
-                    bail!("invalid byte {}", byte)
-                }
-            }
-            Some(arr)
-        } else {
-            let path = input.unwrap();
-            let mut f = File::open(path)?;
-            let mut filelen = f.metadata().unwrap().len() as usize;
-            if offset.is_some() {
-                let offset = offset.unwrap();
-                f.seek(SeekFrom::Start(offset.into()))?;
-                // Assuming seek past end of file does work (TODO: check that),
-                // then it's ok to subtract offset from filelen.
-                filelen -= offset as usize;
-            }
-            if filelen > 65536 {
-                // TODO: There is a 64k limit somewhere
-                // TODO: --data xx,yy,zz,.. could exceed 64k too.
-                info!("Exceeding 64k buffer. Truncating to 64k");
-                filelen = 65536;
-            }
-
-            let mut arr = vec![0u8; filelen as usize];
-            match f.read(&mut arr) {
-                Ok(_) => (),
-                Err(err) => bail!("Error {}: Cannot read", err),
-            }
-            Some(arr)
-        };
-        // Write is page program. spiflash-api can break it down to multiple
-        // operations.
-        // TODO: Use cleaner abstractions that don't conflict with lower-level
-        // semantics.
-
-        // XXX What is the right order to push these Ops?
-        let inst = parse_instruction("page-program").unwrap();
-        ops.push(Op::Push(inst as u8));
-        ops.push(Op::Push32(address));
-
-        if !buffer.is_some() {
-            bail!("SpiFlash write is missing input data to write to flash.");
-        }
-        let buffer = buffer.unwrap();
-
-        if length.is_some() {
-            let length = length.unwrap();
-            if length as usize > buffer.len() {
-                bail!("--length is greater than provided data");
-            }
+        Command::Read { address, length, output } => {
+            // e.g. spiflash read 4000 100 --out ./data.bin
+            println!("address {:?}", address);
+            println!("length {:?}", length);
+            println!("output {:?}", output);
+            // XXX What is the right order to push these Ops?
+            ops.push(Op::Push(parse_instruction("read").unwrap() as u8));
+            ops.push(Op::Push32(address));
             ops.push(Op::Push32(length));
-        } else {
-            ops.push(Op::Push32(buffer.len() as u32));
+            let mut arr = vec![0u8; length as usize];
+            ops.push(Op::Call(spiflash_read.id));
+            Some(arr)
         }
-        ops.push(Op::Call(spiflash_write.id));
-        Some(buffer)
-    },
-    };
+        Command::Write { address, length, offset, input, data } => {
+            // Write data to a SPI flash.
+            //
+            // Hubris drv_spiflash_api handles issuing the WEN instruction before PP.
+            //
+            // e.g. spiflash write [--erase] address <length> \
+            //          [-offset xxx] [--input path] [--data xx,xx,... ]
 
+            // Note: It is assumed that a chip erase has been done and that no page
+            // needs to be erased before programming.
+            // Alternatively, a series of sector erase commands could be issued to
+            // reset bytes in the flash before programming an area.
+            // Some devices have limits on the safety of multiple writes to the same
+            // flash area (line, page, sector) between erases.
+            // TODO: check supported write/erase attributes of the parts being used.
+            println!("address {:?}", address);
+            // If length is not specified, it should be the length of the source.
+            // If length is specified, it should override the length of the source.
+            // If the source has less data than specified by length, that's an error.
+            println!("length {:?}", length);
+
+            // There are limits on buffer size that can be transferred to Hubris,
+            // so, an offset into the source is allowed.
+            // TODO: Add appropriate buffering and looping to allow fully automated
+            // writing of entire flash images.
+            println!("offset {:?}", offset);
+
+            println!("input {:?}", input);
+            println!("data {:?}", data);
+            // Input and Data should not be specified at the same time.
+            if input.is_some() == data.is_some() {
+                bail!("One of '--in path' or '--data bytes' are required, but not both");
+            }
+
+            let buffer = if data.is_some() {
+                // Parse a string of comma separated hex bytes.
+                let data = data.unwrap();
+                let bytes: Vec<&str> = data.split(",").collect();
+                let mut arr = Vec::new();
+
+                for byte in &bytes {
+                    if let Ok(val) = u8::from_str_radix(byte, 16) {
+                        arr.push(val);
+                    } else {
+                        bail!("invalid byte {}", byte)
+                    }
+                }
+                Some(arr)
+            } else {
+                let path = input.unwrap();
+                let mut f = File::open(path)?;
+                let mut filelen = f.metadata().unwrap().len() as usize;
+                if offset.is_some() {
+                    let offset = offset.unwrap();
+                    f.seek(SeekFrom::Start(offset.into()))?;
+                    // Assuming seek past end of file does work (TODO: check that),
+                    // then it's ok to subtract offset from filelen.
+                    filelen -= offset as usize;
+                }
+                if filelen > 65536 {
+                    // TODO: There is a 64k limit somewhere
+                    // TODO: --data xx,yy,zz,.. could exceed 64k too.
+                    info!("Exceeding 64k buffer. Truncating to 64k");
+                    filelen = 65536;
+                }
+
+                let mut arr = vec![0u8; filelen as usize];
+                match f.read(&mut arr) {
+                    Ok(_) => (),
+                    Err(err) => bail!("Error {}: Cannot read", err),
+                }
+                Some(arr)
+            };
+            // Write is page program. spiflash-api can break it down to multiple
+            // operations.
+            // TODO: Use cleaner abstractions that don't conflict with lower-level
+            // semantics.
+
+            // XXX What is the right order to push these Ops?
+            let inst = parse_instruction("page-program").unwrap();
+            ops.push(Op::Push(inst as u8));
+            ops.push(Op::Push32(address));
+
+            if !buffer.is_some() {
+                bail!(
+                    "SpiFlash write is missing input data to write to flash."
+                );
+            }
+            let buffer = buffer.unwrap();
+
+            if length.is_some() {
+                let length = length.unwrap();
+                if length as usize > buffer.len() {
+                    bail!("--length is greater than provided data");
+                }
+                ops.push(Op::Push32(length));
+            } else {
+                ops.push(Op::Push32(buffer.len() as u32));
+            }
+            ops.push(Op::Call(spiflash_write.id));
+            Some(buffer)
+        }
+    };
 
     ops.push(Op::Done);
 
