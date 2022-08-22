0d91119e7e4a6af1679a8e9775ed4ce40aa400f8
Steve Klabnik <steve@steveklabnik.com>
2022-08-22 16:25:12 -0500

    Fix the build on Windows
    Use if_nametoindex from winapi, since it's not in libc on Windows.
    
    Fixes #218

--- cmd/rpc/src/lib.rs
+++ cmd/rpc/src/lib.rs
@@ -204,13 +204,18 @@ fn rpc_listen(hubris: &HubrisArchive, rpc_args: &RpcArgs) -> Result<()> {
 }
 
 fn decode_iface(iface: &str) -> Result<u32> {
+    #[cfg(not(windows))]
+    use libc::if_nametoindex;
+    #[cfg(windows)]
+    use winapi::shared::netioapi::if_nametoindex;
+
     // Work around https://github.com/rust-lang/rust/issues/65976 by manually
     // converting from scopeid to numerical value.  I'm not any happier about
     // this than you are!
     let iface_c = std::ffi::CString::new(iface).unwrap();
     let scopeid: u32 = iface
         .parse()
-        .unwrap_or_else(|_| unsafe { libc::if_nametoindex(iface_c.as_ptr()) });
+        .unwrap_or_else(|_| unsafe { if_nametoindex(iface_c.as_ptr()) });
     if scopeid == 0 {
         bail!("Could not find interface for {}", iface);
     }
