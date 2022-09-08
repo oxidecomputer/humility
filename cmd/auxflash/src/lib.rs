e2f85c417c7235918701946078e9609f5099bd0b
Matt Keeter <matt@oxide.computer>
2022-09-08 15:03:19 +0000

    Testing on an actual Sidecar

--- cmd/auxflash/src/lib.rs
+++ cmd/auxflash/src/lib.rs
@@ -19,7 +19,8 @@ use humility_cmd::{Archive, Attach, Command, Run, Validate};
 use humility_cmd_hiffy::HiffyLease;
 
 const SLOT_SIZE_BYTES: usize = 1024 * 1024;
-const SLOT_CHUNK_SIZE_BYTES: usize = 256;
+const READ_CHUNK_SIZE: usize = 256; // limited by HIFFY_SCRATCH_SIZE
+const WRITE_CHUNK_SIZE: usize = 2048; // limited by HIFFY_DATA_SIZE
 
 #[derive(Parser, Debug)]
 #[clap(name = "auxflash", about = env!("CARGO_PKG_DESCRIPTION"))]
@@ -38,13 +39,24 @@ struct AuxFlashArgs {
 #[derive(Parser, Debug)]
 enum AuxFlashCommand {
     /// Prints the auxiliary flash status
-    Status,
+    Status {
+        #[clap(long, short)]
+        verbose: bool,
+    },
     Read {
+        #[clap(long, short)]
         slot: u32,
+        #[clap(long, short)]
         output: String,
+
+        /// Number of bytes to read (defaults to 1M)
+        #[clap(long, short)]
+        count: Option<usize>,
     },
     Write {
+        #[clap(long, short)]
         slot: u32,
+        #[clap(long, short)]
         input: String,
     },
 }
@@ -67,6 +79,31 @@ fn slot_count(
     };
     let v = v.as_base()?;
     v.as_u32().ok_or_else(|| anyhow!("Couldn't get U32"))
+}
+
+fn slot_erase(
+    hubris: &HubrisArchive,
+    core: &mut dyn Core,
+    context: &mut HiffyContext,
+    slot: u32,
+) -> Result<()> {
+    let op = IdolOperation::new(hubris, "AuxFlash", "erase_slot", None)
+        .context(
+            "Could not find `AuxFlash.erase_slot`, \
+             is your Hubris archive new enough?",
+        )?;
+    let value = humility_cmd_hiffy::hiffy_call(
+        hubris,
+        core,
+        context,
+        &op,
+        &[("slot", IdolArgument::Scalar(u64::from(slot)))],
+        None,
+    )?;
+    match value {
+        Ok(..) => Ok(()),
+        Err(e) => bail!("Got Hiffy error: {}", e),
+    }
 }
 
 fn slot_status(
@@ -92,9 +129,7 @@ fn slot_status(
         Ok(v) => v,
         Err(e) => bail!("{}", e),
     };
-    let v = v.as_struct()?;
-    assert_eq!(v.name(), "AuxFlashChecksum");
-    let array = v["__0"].as_array().unwrap();
+    let array = v.as_1tuple().unwrap().as_array().unwrap();
     assert_eq!(array.len(), 32);
     let mut out = [0u8; 32];
     for (o, v) in out
@@ -111,24 +146,37 @@ fn auxflash_status(
     hubris: &HubrisArchive,
     core: &mut dyn Core,
     context: &mut HiffyContext,
+    verbose: bool,
 ) -> Result<()> {
     let slot_count = slot_count(hubris, core, context)?;
     println!("slot | status");
-    println!("-----|--------------------");
+    println!("-----|----------------------------");
     for i in 0..slot_count {
-        print!(" {:3>} |", i);
+        print!(" {:>3} | ", i);
         match slot_status(hubris, core, context, i) {
             Err(e) => {
                 let err_str = format!("{:?}", e);
-                print!("{}", err_str.red())
-            }
-            Ok(v) => {
-                for byte in v {
-                    print!("{:0>2x}", byte);
+                // Special-casing for a few known error codes
+                match err_str.as_str() {
+                    "MissingChck" => println!("{}", "Missing CHCK".yellow()),
+                    _ => println!("{}", err_str.red()),
                 }
             }
+            Ok(v) => {
+                print!("{} (", "Checkum match".green());
+                if verbose {
+                    for byte in v {
+                        print!("{:0>2x}", byte);
+                    }
+                } else {
+                    for byte in &v[0..4] {
+                        print!("{:0>2x}", byte);
+                    }
+                    print!("...");
+                }
+                println!(")");
+            }
         }
-        println!();
     }
     Ok(())
 }
@@ -138,6 +186,7 @@ fn auxflash_read(
     core: &mut dyn Core,
     context: &mut HiffyContext,
     slot: u32,
+    count: Option<usize>,
 ) -> Result<Vec<u8>> {
     let op =
         IdolOperation::new(hubris, "AuxFlash", "read_slot_with_offset", None)
@@ -146,15 +195,15 @@ fn auxflash_read(
              is your Hubris archive new enough?",
         )?;
 
-    let mut out = vec![0u8; SLOT_SIZE_BYTES];
+    let mut out = vec![0u8; count.unwrap_or(SLOT_SIZE_BYTES)];
     let bar = ProgressBar::new(0);
     bar.set_style(
         ProgressStyle::default_bar()
             .template("humility: reading [{bar:30}] {bytes}/{total_bytes}"),
     );
-    bar.set_length(SLOT_SIZE_BYTES as u64);
-    for (i, chunk) in out.chunks_mut(SLOT_CHUNK_SIZE_BYTES).enumerate() {
-        let offset = i * SLOT_CHUNK_SIZE_BYTES;
+    bar.set_length(out.len() as u64);
+    for (i, chunk) in out.chunks_mut(READ_CHUNK_SIZE).enumerate() {
+        let offset = i * READ_CHUNK_SIZE;
         let value = humility_cmd_hiffy::hiffy_call(
             hubris,
             core,
@@ -182,6 +231,9 @@ fn auxflash_write(
     slot: u32,
     data: &[u8],
 ) -> Result<()> {
+    humility::msg!("Erasing slot {}", slot);
+    slot_erase(hubris, core, context, slot)?;
+
     if data.len() > SLOT_SIZE_BYTES {
         bail!(
             "Data is too large ({} bytes, slot size is {} bytes)",
@@ -199,11 +251,11 @@ fn auxflash_write(
     let bar = ProgressBar::new(0);
     bar.set_style(
         ProgressStyle::default_bar()
-            .template("humility: reading [{bar:30}] {bytes}/{total_bytes}"),
+            .template("humility: writing [{bar:30}] {bytes}/{total_bytes}"),
     );
     bar.set_length(data.len() as u64);
-    for (i, chunk) in data.chunks(SLOT_CHUNK_SIZE_BYTES).enumerate() {
-        let offset = i * SLOT_CHUNK_SIZE_BYTES;
+    for (i, chunk) in data.chunks(WRITE_CHUNK_SIZE).enumerate() {
+        let offset = i * WRITE_CHUNK_SIZE;
         let value = humility_cmd_hiffy::hiffy_call(
             hubris,
             core,
@@ -231,9 +283,11 @@ fn auxflash(
     let subargs = AuxFlashArgs::try_parse_from(subargs)?;
     let mut context = HiffyContext::new(hubris, core, subargs.timeout)?;
     match subargs.cmd {
-        AuxFlashCommand::Status => auxflash_status(hubris, core, &mut context)?,
-        AuxFlashCommand::Read { slot, output } => {
-            let data = auxflash_read(hubris, core, &mut context, slot)?;
+        AuxFlashCommand::Status { verbose } => {
+            auxflash_status(hubris, core, &mut context, verbose)?;
+        }
+        AuxFlashCommand::Read { slot, output, count } => {
+            let data = auxflash_read(hubris, core, &mut context, slot, count)?;
             std::fs::write(&output, &data)?;
         }
         AuxFlashCommand::Write { slot, input } => {
