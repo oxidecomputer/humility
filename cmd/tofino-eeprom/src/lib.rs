a92f65aafbfbc4d26918bc6aa5a9ddc480340e72
Matt Keeter <matt@oxide.computer>
2022-10-19 09:57:49 -0500

    Tofino EEPROM management (#249)
    This PR adds a new subcommand, `humility tofino-eeprom`, which reads and writes
    to the Tofino EEPROM.
    
    It's basically a copy of `humility auxflash`, with "auxflash" crossed out and
    "tofino-eeprom" written in sharpie.

---
+++ cmd/tofino-eeprom/src/lib.rs
@@ -0,0 +1,191 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! ## `humility tofino-eeprom`
+//!
+//! Tools to interact with the Tofino EEPROM
+
+use anyhow::{bail, Context, Result};
+use clap::{Command as ClapCommand, CommandFactory, Parser};
+use humility::cli::Subcommand;
+use indicatif::{ProgressBar, ProgressStyle};
+
+use humility::core::Core;
+use humility::hubris::*;
+use humility_cmd::hiffy::HiffyContext;
+use humility_cmd::idol::IdolArgument;
+use humility_cmd::idol::IdolOperation;
+use humility_cmd::{Archive, Attach, Command, Validate};
+use humility_cmd_hiffy::HiffyLease;
+
+// Limited to 128 bytes due to the write buffer in the EEPROM
+const READ_CHUNK_SIZE: usize = 128;
+const WRITE_CHUNK_SIZE: usize = 128;
+
+const EEPROM_SIZE_BYTES: usize = 65536;
+
+#[derive(Parser, Debug)]
+#[clap(name = "tofino-eeprom", about = env!("CARGO_PKG_DESCRIPTION"))]
+struct EepromArgs {
+    /// sets timeout
+    #[clap(
+        long, short = 'T', default_value = "15000", value_name = "timeout_ms",
+        parse(try_from_str = parse_int::parse)
+    )]
+    timeout: u32,
+
+    #[clap(subcommand)]
+    cmd: EepromCommand,
+}
+
+#[derive(Parser, Debug)]
+enum EepromCommand {
+    /// Reads from the EEPROM, saving data to a file
+    Read {
+        /// Number of bytes to read (defaults to EEPROM size)
+        #[clap(long, short)]
+        count: Option<usize>,
+
+        /// Filename of output
+        output: String,
+    },
+    /// Writes a file to the EEPROM
+    Write {
+        /// Filename of output
+        input: String,
+    },
+}
+
+pub struct EepromHandler<'a> {
+    hubris: &'a HubrisArchive,
+    core: &'a mut dyn Core,
+    context: HiffyContext<'a>,
+}
+
+impl<'a> EepromHandler<'a> {
+    pub fn new(
+        hubris: &'a HubrisArchive,
+        core: &'a mut dyn Core,
+        hiffy_timeout: u32,
+    ) -> Result<Self> {
+        let context = HiffyContext::new(hubris, core, hiffy_timeout)?;
+        Ok(Self { hubris, core, context })
+    }
+
+    fn get_idol_command(&self, name: &str) -> Result<IdolOperation<'a>> {
+        IdolOperation::new(self.hubris, "Sequencer", name, None).with_context(
+            || {
+                format!(
+                    "Could not find `Sequencer.{}`, \
+                     is your Hubris archive new enough?",
+                    name
+                )
+            },
+        )
+    }
+
+    fn eeprom_read(&mut self, count: Option<usize>) -> Result<Vec<u8>> {
+        let op = self.get_idol_command("read_spi_eeprom_bytes")?;
+
+        let mut out = vec![0u8; count.unwrap_or(EEPROM_SIZE_BYTES)];
+        let bar = ProgressBar::new(0);
+        bar.set_style(
+            ProgressStyle::default_bar()
+                .template("humility: reading [{bar:30}] {bytes}/{total_bytes}"),
+        );
+        bar.set_length(out.len() as u64);
+        for (i, chunk) in out.chunks_mut(READ_CHUNK_SIZE).enumerate() {
+            let offset = i * READ_CHUNK_SIZE;
+            let value = humility_cmd_hiffy::hiffy_call(
+                self.hubris,
+                self.core,
+                &mut self.context,
+                &op,
+                &[("offset", IdolArgument::Scalar(offset as u64))],
+                Some(HiffyLease::Read(chunk)),
+            )?;
+            if let Err(e) = value {
+                bail!("Got Hubris error: {:?}", e);
+            }
+            bar.set_position(offset as u64);
+        }
+
+        Ok(out)
+    }
+
+    pub fn eeprom_write(&mut self, data: &[u8]) -> Result<()> {
+        if data.len() > EEPROM_SIZE_BYTES {
+            bail!(
+                "Data is too large ({} bytes, eeprom size is {} bytes)",
+                data.len(),
+                EEPROM_SIZE_BYTES
+            );
+        }
+        let op = self.get_idol_command("write_spi_eeprom_bytes")?;
+
+        let bar = ProgressBar::new(0);
+        bar.set_style(
+            ProgressStyle::default_bar()
+                .template("humility: writing [{bar:30}] {bytes}/{total_bytes}"),
+        );
+        bar.set_length(data.len() as u64);
+        for (i, chunk) in data.chunks(WRITE_CHUNK_SIZE).enumerate() {
+            let offset = i * WRITE_CHUNK_SIZE;
+            let value = humility_cmd_hiffy::hiffy_call(
+                self.hubris,
+                self.core,
+                &mut self.context,
+                &op,
+                &[("offset", IdolArgument::Scalar(offset as u64))],
+                Some(HiffyLease::Write(chunk)),
+            )?;
+            if let Err(e) = value {
+                bail!("Got Hubris error: {:?}", e);
+            }
+            bar.set_position(offset as u64);
+        }
+        bar.set_position(data.len() as u64);
+        humility::msg!("done");
+        Ok(())
+    }
+
+    pub fn reset(&mut self) -> Result<()> {
+        self.core.reset()
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+fn eeprom(context: &mut humility::ExecutionContext) -> Result<()> {
+    let core = &mut **context.core.as_mut().unwrap();
+    let Subcommand::Other(subargs) = context.cli.cmd.as_ref().unwrap();
+    let subargs = EepromArgs::try_parse_from(subargs)?;
+    let hubris = context.archive.as_ref().unwrap();
+    let mut worker = EepromHandler::new(hubris, core, subargs.timeout)?;
+
+    match subargs.cmd {
+        EepromCommand::Read { output, count } => {
+            let data = worker.eeprom_read(count)?;
+            std::fs::write(&output, &data)?;
+        }
+        EepromCommand::Write { input } => {
+            let data = std::fs::read(&input)?;
+            worker.eeprom_write(&data)?;
+        }
+    }
+    Ok(())
+}
+
+pub fn init() -> (Command, ClapCommand<'static>) {
+    (
+        Command::Attached {
+            name: "tofino-eeprom",
+            archive: Archive::Required,
+            attach: Attach::LiveOnly,
+            validate: Validate::Booted,
+            run: eeprom,
+        },
+        EepromArgs::command(),
+    )
+}
