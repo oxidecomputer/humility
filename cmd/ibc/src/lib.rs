e8273f1f8c672632e61f4df1ef290d8dc929c592
Matt Keeter <matt@oxide.computer>
2023-02-01 20:59:59 +0000

    Lots of work on Humility IBC

--- cmd/ibc/src/lib.rs
+++ cmd/ibc/src/lib.rs
@@ -11,17 +11,18 @@ use clap::{CommandFactory, Parser};
 use colored::Colorize;
 use humility::cli::Subcommand;
 use humility_cmd::CommandKind;
+use zerocopy::{
+    byteorder::{BigEndian, U16, U32},
+    AsBytes, FromBytes,
+};
 
-use cmd_hiffy as humility_cmd_hiffy;
 use hif::*;
 
 use humility::core::Core;
 use humility::hubris::*;
 use humility_cmd::hiffy::HiffyContext;
 use humility_cmd::i2c::I2cArgs;
-use humility_cmd::idol::{HubrisIdol, IdolArgument};
 use humility_cmd::{Archive, Attach, Command, Validate};
-use humility_cmd_hiffy::HiffyLease;
 
 #[derive(Parser, Debug)]
 #[clap(name = "ibc", about = env!("CARGO_PKG_DESCRIPTION"))]
@@ -47,7 +48,6 @@ enum IbcSubcommand {
 }
 
 pub struct IbcHandler<'a> {
-    hubris: &'a HubrisArchive,
     device: &'a HubrisI2cDevice,
     core: &'a mut dyn Core,
     context: HiffyContext<'a>,
@@ -67,13 +67,9 @@ impl<'a> IbcHandler<'a> {
             .ok_or_else(|| anyhow!("could not find 'bmr491' device"))?;
         println!("{device:?}");
         let context = HiffyContext::new(hubris, core, hiffy_timeout)?;
-        Ok(Self { hubris, device, core, context })
+        Ok(Self { device, core, context })
     }
     pub fn blackbox(&mut self, verbose: bool) -> Result<()> {
-        if verbose {
-            humility::msg!("hi");
-        }
-
         let funcs = self.context.functions()?;
         let write_func = funcs.get("I2cWrite", 8)?;
         let read_func = funcs.get("I2cRead", 7)?;
@@ -84,8 +80,6 @@ impl<'a> IbcHandler<'a> {
             pmbus::commands::bmr491::CommandCode::MFR_EVENT_INDEX as u8;
         const READ_EVENT: u8 =
             pmbus::commands::bmr491::CommandCode::MFR_READ_EVENT as u8;
-
-        // We want to loop over the 48 slots (24 fault, 24 event)
 
         let mut ops = vec![];
         ops.push(Op::Push(hargs.controller));
@@ -99,7 +93,39 @@ impl<'a> IbcHandler<'a> {
         }
         ops.push(Op::Push(hargs.address.unwrap()));
 
-        // Let's unroll the loop here to avoid having to think about it in HIF
+        // We must read VOUT_MODE to interpret later data
+        ops.push(Op::Push(
+            pmbus::commands::bmr491::CommandCode::VOUT_MODE as u8,
+        ));
+        ops.push(Op::Push(1)); // Number of bytes to read
+        ops.push(Op::Call(read_func.id));
+        ops.push(Op::DropN(2));
+
+        // The IBC contains 48 slots in its black box.  The lower 24 are fault
+        // slots, and the upper 24 are life-cycle events.  By writing special
+        // indexes to EVENT_INDEX, we can read back the index of the newest
+        // record in each section
+        ops.push(Op::Push(EVENT_INDEX));
+        ops.push(Op::Push(255)); // Special to get newest fault index
+        ops.push(Op::Push(1)); // Number of words to write
+        ops.push(Op::Call(write_func.id));
+        ops.push(Op::DropN(2));
+
+        ops.push(Op::Push(1)); // Number of bytes to read
+        ops.push(Op::Call(read_func.id));
+        ops.push(Op::DropN(1));
+
+        ops.push(Op::Push(254)); // Special to get newest life-cycle index
+        ops.push(Op::Push(1)); // Number of words to write
+        ops.push(Op::Call(write_func.id));
+        ops.push(Op::DropN(2));
+
+        ops.push(Op::Push(1)); // Number of bytes to read
+        ops.push(Op::Call(read_func.id));
+        ops.push(Op::DropN(2));
+
+        // Now, read all of the actual events!
+        // We'll unroll the loop here to avoid having to think about it in HIF
         for i in 0..48 {
             ops.push(Op::Push(EVENT_INDEX));
             ops.push(Op::Push(i)); // This is our actual index
@@ -117,9 +143,144 @@ impl<'a> IbcHandler<'a> {
         ops.push(Op::Done);
 
         let results = self.context.run(self.core, ops.as_slice(), None)?;
-        println!("{results:?}");
+
+        let vout_mode = results[0].as_ref().unwrap()[0];
+
+        // Results are alternating between writing EVENT_INDEX and reading
+        // READ_EVENT
+        for (i, r) in results.iter().skip(1).step_by(2).enumerate() {
+            if let Err(e) = r {
+                bail!("Failed to read event {i}: {e}");
+            }
+        }
+        let newest_fault_event_index = results[2].as_ref().unwrap()[0];
+        let newest_lifecycle_event_index = results[4].as_ref().unwrap()[0];
+
+        let mut events = vec![];
+        for (i, r) in results.iter().skip(6).step_by(2).enumerate() {
+            match r {
+                Ok(r) => {
+                    events.push(IbcEvent::read_from(&r[1..]).ok_or_else(
+                        || anyhow!("Failed to decode IbcEvent from {r:?}"),
+                    )?);
+                }
+                Err(e) => bail!("Failed to read event {i}: {e}"),
+            }
+        }
+        for i in 0..=newest_fault_event_index {
+            let e = events[i as usize];
+            println!("{}", "FAULT EVENT".red());
+            println!("  EVENT_INDEX:        {i}");
+            self.print_event(e, verbose, vout_mode);
+        }
+        for i in 24..=newest_lifecycle_event_index {
+            let e = events[i as usize];
+            println!("{}", "LIFECYCLE EVENT".green());
+            println!("  EVENT_INDEX:        {i}");
+            self.print_event(e, verbose, vout_mode);
+        }
 
         Ok(())
+    }
+
+    fn print_event(&self, e: IbcEvent, verbose: bool, vout_mode: u8) {
+        println!(
+            "  TIMESTAMP           {:#010x} = {}",
+            e.timestamp.get(),
+            Self::format_time(e.timestamp.get())
+        );
+        println!("  EVENT_ID            {:#06x}", e.event_id.get());
+        println!("  STATUS_WORD         {:#06x}", e.status_word.get());
+        let statuses = [
+            ("STATUS_VOUT", e.status_vout),
+            ("STATUS_IOUT", e.status_iout),
+            ("STATUS_INPUT", e.status_input),
+            ("STATUS_TEMPERATURE", e.status_temperature),
+            ("STATUS_CML", e.status_cml),
+            ("STATUS_OTHER", e.status_other),
+            ("STATUS_MFG", e.status_mfr),
+        ];
+
+        for (name, value) in statuses {
+            if value == 0 && !verbose {
+                continue;
+            }
+            print!("  {: <18}  ", name);
+            let v = format!("{:#06x}", value);
+            if value == 0 {
+                println!("{}", v.dimmed());
+            } else {
+                println!("{v}");
+            }
+        }
+        if e.status_word.get() == 0x0001 {
+            for (i, flag) in [
+                "BOOT_EVENT",
+                "INPUT_LOW_EVENT",
+                "CANCEL_EVENT",
+                "ERASE_EVENT",
+                "CLR_EVENT",
+                "ERASE_OVFL_EVENT",
+            ]
+            .iter()
+            .enumerate()
+            {
+                // TODO: some of these flags are only valid in the life-cycle
+                // section
+                if e.status_mfr & (1 << i) != 0 {
+                    println!("    b{i} = {flag}");
+                }
+            }
+        }
+
+        let dev = pmbus::commands::Device::Bmr491;
+        use pmbus::commands::bmr491::CommandCode;
+        let vout_mode_cb =
+            || pmbus::commands::VOUT_MODE::CommandData(vout_mode);
+
+        let values = [
+            ("V_IN", CommandCode::READ_VIN, e.vin_value.get()),
+            ("V_OUT", CommandCode::READ_VOUT, e.vout_value.get()),
+            ("I_OUT", CommandCode::READ_IOUT, e.iout_value.get()),
+            (
+                "TEMPERATURE",
+                CommandCode::READ_TEMPERATURE_1,
+                e.temperature_value.get(),
+            ),
+        ];
+
+        for (name, command, value) in values {
+            dev.interpret(
+                command as u8,
+                value.as_bytes(),
+                vout_mode_cb,
+                |_field, value| {
+                    println!("  {: <18}  {:#06x} = {value}", name, value.raw())
+                },
+            )
+            .unwrap();
+        }
+    }
+
+    fn format_time(timestamp: u32) -> String {
+        let time_sec = timestamp / 10;
+        let seconds = time_sec % 60;
+        let minutes = (time_sec / 60) % 60;
+        let hours = (time_sec / 3600) % 24;
+        let days = time_sec / (3600 * 24);
+        let mut out = String::new();
+        if days > 0 {
+            out += &format!("{days} day, ");
+        }
+        if hours > 0 {
+            out += &format!("{hours} hr, ");
+        }
+        if minutes > 0 {
+            out += &format!("{minutes} min, ");
+        }
+        out += &format!("{seconds}.{} sec", time_sec % 10);
+
+        out
     }
 }
 
@@ -131,10 +292,11 @@ impl<'a> IbcHandler<'a> {
 /// any further.
 ///
 /// See page 19 of the BMR491 technical specification
+#[derive(Copy, Clone, Debug, FromBytes)]
 struct IbcEvent {
-    event_id: u16,
-    timestamp: u32,
-    status_word: u16,
+    event_id: U16<BigEndian>,
+    timestamp: U32<BigEndian>,
+    status_word: U16<BigEndian>,
     status_vout: u8,
     status_iout: u8,
     status_input: u8,
@@ -142,10 +304,10 @@ struct IbcEvent {
     status_cml: u8,
     status_other: u8,
     status_mfr: u8,
-    vin_value: u16,
-    vout_value: u16,
-    iout_value: u16,
-    temperature_value: u16,
+    vin_value: U16<BigEndian>,
+    vout_value: U16<BigEndian>,
+    iout_value: U16<BigEndian>,
+    temperature_value: U16<BigEndian>,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -172,7 +334,7 @@ pub fn init() -> Command {
         run: ibc,
         kind: CommandKind::Attached {
             archive: Archive::Required,
-            attach: Attach::Any, // TODO
+            attach: Attach::LiveOnly,
             validate: Validate::Booted,
         },
     }
