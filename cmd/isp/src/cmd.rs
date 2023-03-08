a13551b7b8ca2cd6b77d8eb407f90c2921723924
Cliff L. Biffle <cliff@oxide.computer>
2023-03-08 15:06:45 -0800

    cmd/isp: allow slices in more places
    There were some internal utility routines here that were requiring Vecs
    to be passed in by value, which means whatever they're given has to be
    in a Vec in the first place, and may need to be copied to call them.
    
    Slices are strictly more general; switched every case I could find.

--- cmd/isp/src/cmd.rs
+++ cmd/isp/src/cmd.rs
@@ -9,9 +9,9 @@ fn do_command(
     port: &mut dyn serialport::SerialPort,
     tag: CommandTag,
     command_resp: ResponseCode,
-    args: Vec<u32>,
+    args: &[u32],
 ) -> Result<Vec<u32>> {
-    send_command(port, tag, &args)?;
+    send_command(port, tag, args)?;
 
     read_response(port, command_resp)
 }
@@ -37,7 +37,7 @@ fn recv_data(
 }
 
 pub fn do_save_keystore(port: &mut dyn serialport::SerialPort) -> Result<()> {
-    let args = vec![
+    let args = [
         // Arg 0 =  WriteNonVolatile
         KeyProvisionCmds::WriteNonVolatile as u32,
         // Arg 1 = Memory ID (0 = internal flash)
@@ -48,14 +48,14 @@ pub fn do_save_keystore(port: &mut dyn serialport::SerialPort) -> Result<()> {
         port,
         CommandTag::KeyProvision,
         ResponseCode::Generic,
-        args,
+        &args,
     )?;
 
     Ok(())
 }
 
 pub fn do_enroll(port: &mut dyn serialport::SerialPort) -> Result<()> {
-    let args = vec![
+    let args = [
         // Arg =  Enroll
         KeyProvisionCmds::Enroll as u32,
     ];
@@ -64,14 +64,14 @@ pub fn do_enroll(port: &mut dyn serialport::SerialPort) -> Result<()> {
         port,
         CommandTag::KeyProvision,
         ResponseCode::Generic,
-        args,
+        &args,
     )?;
 
     Ok(())
 }
 
 pub fn do_generate_uds(port: &mut dyn serialport::SerialPort) -> Result<()> {
-    let args = vec![
+    let args = [
         // Arg 0 =  SetIntrinsicKey
         KeyProvisionCmds::SetIntrinsicKey as u32,
         // Arg 1 = UDS
@@ -84,7 +84,7 @@ pub fn do_generate_uds(port: &mut dyn serialport::SerialPort) -> Result<()> {
         port,
         CommandTag::KeyProvision,
         ResponseCode::Generic,
-        args,
+        &args,
     )?;
 
     Ok(())
@@ -92,18 +92,18 @@ pub fn do_generate_uds(port: &mut dyn serialport::SerialPort) -> Result<()> {
 
 pub fn do_isp_write_keystore(
     port: &mut dyn serialport::SerialPort,
-    data: Vec<u8>,
+    data: &[u8],
 ) -> Result<()> {
-    let args = vec![KeyProvisionCmds::WriteKeyStore as u32];
+    let args = [KeyProvisionCmds::WriteKeyStore as u32];
 
     let _ = do_command(
         port,
         CommandTag::KeyProvision,
         ResponseCode::KeyProvision,
-        args,
+        &args,
     )?;
 
-    send_data(port, &data, ResponseCode::Generic)
+    send_data(port, data, ResponseCode::Generic)
 }
 
 pub fn do_isp_read_memory(
@@ -111,7 +111,7 @@ pub fn do_isp_read_memory(
     address: u32,
     cnt: u32,
 ) -> Result<Vec<u8>> {
-    let args = vec![
+    let args = [
         // Arg0 = address
         address, // Arg1 = length
         cnt,     // Arg2 = memory type
@@ -122,7 +122,7 @@ pub fn do_isp_read_memory(
         port,
         CommandTag::ReadMemory,
         ResponseCode::ReadMemory,
-        args,
+        &args,
     )?;
 
     recv_data(port, cnt, ResponseCode::Generic)
@@ -131,9 +131,9 @@ pub fn do_isp_read_memory(
 pub fn do_isp_write_memory(
     port: &mut dyn serialport::SerialPort,
     address: u32,
-    data: Vec<u8>,
+    data: &[u8],
 ) -> Result<()> {
-    let args = vec![
+    let args = [
         // arg 0 = address
         address,
         // arg 1 = len
@@ -142,16 +142,20 @@ pub fn do_isp_write_memory(
         0x0_u32,
     ];
 
-    let _ =
-        do_command(port, CommandTag::WriteMemory, ResponseCode::Generic, args)?;
+    let _ = do_command(
+        port,
+        CommandTag::WriteMemory,
+        ResponseCode::Generic,
+        &args,
+    )?;
 
-    send_data(port, &data, ResponseCode::Generic)
+    send_data(port, data, ResponseCode::Generic)
 }
 
 pub fn do_isp_flash_erase_all(
     port: &mut dyn serialport::SerialPort,
 ) -> Result<()> {
-    let args = vec![
+    let args = [
         // Erase internal flash
         0x0_u32,
     ];
@@ -160,7 +164,7 @@ pub fn do_isp_flash_erase_all(
         port,
         CommandTag::FlashEraseAll,
         ResponseCode::Generic,
-        args,
+        &args,
     )?;
 
     Ok(())
@@ -170,25 +174,25 @@ pub fn do_isp_get_property(
     port: &mut dyn serialport::SerialPort,
     prop: BootloaderProperty,
 ) -> Result<Vec<u32>> {
-    let args = vec![
+    let args = [
         // Arg 0 = property
         prop as u32,
     ];
 
-    do_command(port, CommandTag::GetProperty, ResponseCode::GetProperty, args)
+    do_command(port, CommandTag::GetProperty, ResponseCode::GetProperty, &args)
 }
 
 pub fn do_isp_last_error(
     port: &mut dyn serialport::SerialPort,
 ) -> Result<Vec<u32>> {
-    let args = vec![
+    let args = [
         // Arg 0 = LastCRC
         BootloaderProperty::CRCStatus as u32,
         // Arg 1 = Last error
         1_u32,
     ];
 
-    do_command(port, CommandTag::GetProperty, ResponseCode::GetProperty, args)
+    do_command(port, CommandTag::GetProperty, ResponseCode::GetProperty, &args)
 }
 
 pub fn do_ping(port: &mut dyn serialport::SerialPort) -> Result<()> {
