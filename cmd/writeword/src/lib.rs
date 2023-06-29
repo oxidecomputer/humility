a42ca0bdcb231eece1016dae693f5ed334bccd02
Bryan Cantrill <bryan@oxide.computer>
2023-06-28 19:03:52 -0700

    docs

--- cmd/writeword/src/lib.rs
+++ cmd/writeword/src/lib.rs
@@ -4,9 +4,47 @@
 
 //! ## `humility writeword`
 //!
-//! To the specified word-aligned address, writes the specified 32-bit value.
+//! Given a word-aligned address, writes the specified 32-bit value.
 //! If multiple values are specified, writes each in turn, incrementing
 //! the address by the word size after each write.
+//!
+//! **It should go without saying that this should be only used with care.**
+//! In particular, the target is **not** halted before any writes (but that
+//! functionality can be affected by using `writeword`).
+//!
+//! For example, to write the value 0x11223344 to 0x24018900:
+//!
+//! ```console
+//! $ humility writeword 0x24018900 0x11223344
+//! humility: attached via ST-Link V3
+//! humility: writing 0x11223344 to 0x24018900
+//! ```
+//!
+//! Note that the word is written as single, 32-bit value -- and will therefore
+//! be little-endian if reading memory:
+//!
+//! ```console
+//! $ humility readmem 0x24018900 16
+//! humility: attached via ST-Link V3
+//!              \/  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+//! 0x24018900 | 44 33 22 11 00 00 00 00 00 00 00 00 00 00 00 00 | D3".............
+//! ```
+//!
+//! To write multiple values to multiple words starting at the specified
+//! address, specify them as additional arguments, e.g.:
+//!
+//! ```console
+//! $ humility writeword 0x24047800 0xaa 0xbb 0xcc 0xdd
+//! humility: attached via ST-Link V3
+//! humility: writing 0xaa to 0x24047800
+//! humility: writing 0xbb to 0x24047804
+//! humility: writing 0xcc to 0x24047808
+//! humility: writing 0xdd to 0x2404780c
+//! $ humility readmem 0x24047800 16
+//! humility: attached via ST-Link V3
+//!             \/  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+//! 0x24047800 | aa 00 00 00 bb 00 00 00 cc 00 00 00 dd 00 00 00 | ................
+//! ```
 //!
 
 use anyhow::{bail, Result};
