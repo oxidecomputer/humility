a2ec8deca3b7d79f6cff3d7979ab23a645e897a0
Bryan Cantrill <bryan@oxide.computer>
2023-11-02 00:10:51 -0700

    add more BMR491 decoding

--- cmd/ibc/src/lib.rs
+++ cmd/ibc/src/lib.rs
@@ -16,55 +16,89 @@
 //!
 //! Here's an example:
 //! ```console
-//! $ humility -tsn5 ibc black-box
+//! $ humility ibc black-box
 //! humility: attached to 0483:374e:001B00083156501320323443 via ST-Link V3
 //! FAULT EVENT
 //!   EVENT_INDEX:        0
-//!   TIMESTAMP           0x000001ca = 45.5 sec
+//!   TIMESTAMP           0x000a3747 = 18 hr, 35 min, 51.1 sec
 //!   EVENT_ID            0x0000
 //!   STATUS_WORD         0x0010
-//!   STATUS_IOUT         0x0080
-//!   V_IN                0xf869 = 52.50V
-//!   V_OUT               0x5f00 = 11.88V
-//!   I_OUT               0x004e = 78.00A
-//!   TEMPERATURE         0x001c = 28.00°C
+//!     0x0010: Output overcurrent fault
+//!   STATUS_IOUT         0x80
+//!     0x80: Iout Overcurrent Fault
+//!   STATUS_MFR          0x00
+//!   V_IN                0xf84f = 39.500V
+//!   V_OUT               0x6000 = 12.000V
+//!   I_OUT               0x0075 = 117.000A
+//!   TEMPERATURE         0x0023 = 35.000°C
 //! FAULT EVENT
 //!   EVENT_INDEX:        1
-//!   TIMESTAMP           0x000001d4 = 46.6 sec
+//!   TIMESTAMP           0x000a3751 = 18 hr, 35 min, 52.1 sec
 //!   EVENT_ID            0x0001
 //!   STATUS_WORD         0x0001
-//!   STATUS_MFG          0x0001
-//!     b0 = BOOT_EVENT
-//!   V_IN                0xf83c = 30.00V
-//!   V_OUT               0x0000 = 0.00V
-//!   I_OUT               0x0000 = 0.00A
-//!   TEMPERATURE         0x0000 = 0.00°C
+//!     0x0001: System event
+//!   STATUS_MFR          0x01
+//!     0x01: INPUT_LOW_EVENT
+//!   V_IN                0xf83c = 30.000V
+//!   V_OUT               0x0000 = 0.000V
+//!   I_OUT               0x0000 = 0.000A
+//!   TEMPERATURE         0x0000 = 0.000°C
 //! FAULT EVENT
 //!   EVENT_INDEX:        2
-//!   TIMESTAMP           0x000002c4 = 1 min, 10.0 sec
+//!   TIMESTAMP           0x2809e751 = 777 day, 11 hr, 22 min, 48.1 sec
 //!   EVENT_ID            0x0002
-//!   STATUS_WORD         0x0010
-//!   STATUS_IOUT         0x0080
-//!   V_IN                0xf877 = 59.50V
-//!   V_OUT               0x5f00 = 11.88V
-//!   I_OUT               0x0070 = 112.00A
-//!   TEMPERATURE         0x002c = 44.00°C
+//!   STATUS_WORD         0x0001
+//!     0x0001: System event
+//!   STATUS_MFR          0x02
+//!     0x02: CANCEL_EVENT
+//!   V_IN                0xf86b = 53.500V
+//!   V_OUT               0x5f00 = 11.875V
+//!   I_OUT               0x0000 = 0.000A
+//!   TEMPERATURE         0x0012 = 18.000°C
 //! FAULT EVENT
 //!   EVENT_INDEX:        3
-//!   TIMESTAMP           0x27ffb2c4 = 776 day, 16 hr, 48 min, 6.6 sec
+//!   TIMESTAMP           0x50099751 = 1554 day, 4 hr, 9 min, 44.1 sec
 //!   EVENT_ID            0x0003
 //!   STATUS_WORD         0x0001
-//!   STATUS_MFG          0x0002
-//!     b1 = INPUT_LOW_EVENT
-//!   V_IN                0xf86b = 53.50V
-//!   V_OUT               0x6000 = 12.00V
-//!   I_OUT               0x0000 = 0.00A
-//!   TEMPERATURE         0x0016 = 22.00°C
+//!     0x0001: System event
+//!   STATUS_MFR          0x02
+//!     0x02: CANCEL_EVENT
+//!   V_IN                0xf86b = 53.500V
+//!   V_OUT               0x6000 = 12.000V
+//!   I_OUT               0x0000 = 0.000A
+//!   TEMPERATURE         0x0020 = 32.000°C
+//! LIFECYCLE EVENT
+//!   EVENT_INDEX:        24
+//!   TIMESTAMP           0xffef8ad0 = 4969 day, 18 hr, 41 min, 12.0 sec
+//!   EVENT_ID            0x0680
+//!   STATUS_WORD         0x0001
+//!     0x0001: System event
+//!   STATUS_MFR          0x04
+//!     0x04: CLR_EVENT
+//!   V_IN                0xf86b = 53.500V
+//!   V_OUT               0x5f00 = 11.875V
+//!   I_OUT               0x0000 = 0.000A
+//!   TEMPERATURE         0x001a = 26.000°C
+//! LIFECYCLE EVENT
+//!   EVENT_INDEX:        25
+//!   TIMESTAMP           0xffef8bd4 = 4969 day, 18 hr, 41 min, 38.0 sec
+//!   EVENT_ID            0x0681
+//!   STATUS_WORD         0x0001
+//!     0x0001: System event
+//!   STATUS_MFR          0x05
+//!     0x05: ERASE_OVFL_EVENT
+//!   V_IN                0xf860 = 48.000V
+//!   V_OUT               0x5f00 = 11.875V
+//!   I_OUT               0x0000 = 0.000A
+//!   TEMPERATURE         0x001b = 27.000°C
+//!
 //! ```
 //!
-//! The log doesn't appear to be _completely_ reliable, so take it with a grain
-//! of salt and with the datasheet close at hand.  For example, the machine in
-//! the example above had **not** be up for 776 days.
+//! The log doesn't appear to be _completely_ reliable, especially with
+//! respect to timestamps, so take it with a grain of salt and with the
+//! datasheet close at hand.  For example, the machine in the example above
+//! had **not** be up for 777 days (or, for that matter, for 4,969).
+//!
 
 use anyhow::{anyhow, bail, Result};
 use clap::{CommandFactory, Parser};
@@ -203,14 +237,22 @@ impl<'a> IbcHandler<'a> {
 
         // Decoding is extracted from revision A and B of the datasheet, which
         // contain non-overlapping sets of decoding information.  They don't
-        // actively disagree with each other, though, which is good!
+        // actively disagree with each other, though, which is good!  They do,
+        // however, both disagree (if slightly) with the application note that
+        // covers the event recording present in the BMR491 (Application
+        // Note 326, Event recorder for digital Intermediate Bus Converters),
+        // specifically with respect to decoding STATUS_MFR.
         //
-        // 28701-BMR491 02 Rev A, April 2021
-        // 28701-BMR491 10 Rev B, September 2022
+        // The three documents that we're working from:
+        //
+        //   28701-BMR491 02 Rev A, April 2021
+        //   28701-BMR491 10 Rev B, September 2022
+        //   28710-FGB 100 378 Rev A (Application Note 326), May 2023
+
         let status_word = e.status_word.get();
         println!("  STATUS_WORD         {:#06x}", status_word);
         for (bit, name) in [
-            (0, "Unspecified fault (manufacturer?)"),
+            (0, "System event"),
             (1, "CML fault"),
             (2, "Temperature fault or warning"),
             (3, "Input undervoltage fault"),
@@ -218,7 +260,7 @@ impl<'a> IbcHandler<'a> {
             (5, "Output overvoltage fault"),
             (6, "Unit is not providing power"),
             (9, "A bit in Status-Other is set"),
-            (11, "Power-Good signal, if present, is negated"),
+            (11, "Power-Good signal"),
             (12, "Manufacturer specific fault or warning"),
             (
                 13,
@@ -289,29 +331,14 @@ impl<'a> IbcHandler<'a> {
                 ],
             ),
             ("STATUS_OTHER", e.status_other, &[]),
-            (
-                "STATUS_MFG",
-                e.status_mfr,
-                &[
-                    // TODO: some of these flags are only valid in the
-                    // life-cycle section
-                    (0, "BOOT_EVENT"),
-                    (1, "INPUT_LOW_EVENT"),
-                    (2, "CANCEL_EVENT"),
-                    (3, "ERASE_EVENT"),
-                    (4, "CLR_EVENT"),
-                    (5, "ERASE_OVFL_EVENT*"),
-                    (7, "Brown out detected (?)"),
-                ],
-            ),
         ];
 
         for (name, value, bits) in statuses {
             if value == 0 && !verbose {
                 continue;
             }
-            print!("  {: <18}  ", name);
             let v = format!("{:#04x}", value);
+            print!("  {: <18}  ", name);
             if value == 0 {
                 println!("{}", v.dimmed());
             } else {
@@ -320,6 +347,49 @@ impl<'a> IbcHandler<'a> {
             for (bit, name) in bits {
                 if value & (1 << bit) != 0 {
                     println!("    {:#04x}: {name}", 1 << bit);
+                }
+            }
+        }
+
+        // The STATUS_MFR depends on the value of STATUS_WORD.  See
+        // Application Note 321 for details.
+        let mfr = [
+            (
+                0x0001u16,
+                &[
+                    (0u8, "BOOT_EVENT"),
+                    (1, "INPUT_LOW_EVENT"),
+                    (2, "CANCEL_EVENT"),
+                    (3, "ERASE_EVENT"),
+                    (4, "CLR_EVENT"),
+                    (5, "ERASE_OVFL_EVENT"),
+                    (6, "TIME_ERASE_EVENT"),
+                    (7, "Brown out detected (?)"),
+                ] as &[(u8, _)],
+            ),
+            (
+                0x0040,
+                &[
+                    (0, "Unit is off due to internal configuration"),
+                    (1, "Unit is off due to primary remote control"),
+                    (2, "Unit is off due to seconday remote control"),
+                    (3, "Unit is off due to PMBus command"),
+                    (4, "Unit is on due to internal configuration"),
+                    (5, "Unit is on due to primary remote control"),
+                    (6, "Unit is on due to seconday remote control"),
+                    (7, "Unit is on due to PMBus command"),
+                ],
+            ),
+            (0x0800, &[(0, "Power not good"), (1, "Power is good")]),
+        ];
+
+        // Always print the actual raw bits, even if they are 0.
+        println!("  {: <18}  {:#04x}", "STATUS_MFR", e.status_mfr);
+
+        if let Some(&(_, bits)) = mfr.iter().find(|&&(v, _)| v == status_word) {
+            for (value, meaning) in bits {
+                if e.status_mfr == *value {
+                    println!("    {value:#04x}: {meaning}");
                 }
             }
         }
@@ -369,7 +439,7 @@ impl<'a> IbcHandler<'a> {
         if minutes > 0 {
             out += &format!("{minutes} min, ");
         }
-        out += &format!("{seconds}.{} sec", time_sec % 10);
+        out += &format!("{seconds}.{} sec", timestamp % 10);
 
         out
     }
