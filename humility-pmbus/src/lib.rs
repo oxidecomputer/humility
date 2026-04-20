c936b9742613adc093488f5c1389aa4f06d1526b
Matt Keeter <matt@oxide.computer>
2026-04-20 11:03:10 -0400

    Use reflect in pmbus

--- humility-pmbus/src/lib.rs
+++ humility-pmbus/src/lib.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use anyhow::{Result, anyhow, bail};
+use anyhow::{Result, anyhow};
 use humility::{core::Core, hubris::*};
 use std::collections::BTreeMap;
 
@@ -17,40 +17,16 @@ pub fn sensor_id_map(
         .qualified_variables()
         .find(|&(n, _v)| n == "task_power::bsp::CONTROLLER_CONFIG")
         .ok_or_else(|| anyhow!("could not find CONTROLLER_CONFIG"))?;
-    let mut buf: Vec<u8> = vec![0u8; var.size];
-    core.halt()?;
-    core.read_8(var.addr, buf.as_mut_slice())?;
-    core.run()?;
-    let controller_config = humility::reflect::load_value(
-        hubris,
-        &buf,
-        hubris.lookup_type(var.goff)?,
-        0,
-    )?;
+    let array: humility::reflect::Array =
+        humility::reflect::load_variable(hubris, core, var)?;
 
     // Destructure CONTROLLER_CONFIG to build a map of voltage sensor IDs
     // (which we can easily compute) to index in the CONTROLLER_CONFIG
     // array.
-    use humility::reflect::{Base, Value};
-    let Value::Array(array) = controller_config else {
-        bail!("invalid shape for CONTROLLER_CONFIG: {controller_config:?}");
-    };
     let mut sensor_id_to_index = BTreeMap::new();
     for (i, cfg) in array.iter().enumerate() {
-        let Value::Struct(s) = cfg else {
-            bail!("invalid shape for CONTROLLER_CONFIG[{i}]: {cfg:?}");
-        };
-        let v = &s["voltage"];
-        let Value::Tuple(t) = v else {
-            bail!("could not get 'voltage': {v:?}");
-        };
-        let Value::Base(b) = &t[0] else {
-            bail!("could not get sensor ID from 'voltage': {v:?}");
-        };
-        let Base::U32(sensor_id) = b else {
-            bail!("bad sensor id type: {b:?}");
-        };
-        sensor_id_to_index.insert(*sensor_id, i);
+        let sensor_id: u32 = cfg.field("voltage.0")?;
+        sensor_id_to_index.insert(sensor_id, i);
     }
     Ok(sensor_id_to_index)
 }
