97c9748876c63847dbd76f648893fbc375c844fe
Laura Abbott <laura@oxide.computer>
2026-05-21 12:21:13 -0400

    Try the extension trait

--- humility-vpd-lib/examples/list.rs
+++ humility-vpd-lib/examples/list.rs
@@ -3,6 +3,7 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use humility::hubris::HubrisArchive;
+use humility_probes_core::HubrisAttach;
 
 fn main() {
     let hubris = std::env::var("HUMILITY_ARCHIVE").unwrap();
@@ -10,12 +11,7 @@ fn main() {
 
     let hubris = HubrisArchive::load_from_path(&hubris).unwrap();
 
-    let core = &mut *humility_probes_core::attach_to_chip(
-        &probe,
-        Some("STM32H753ZITx"),
-        None,
-    )
-    .unwrap();
+    let core = &mut *hubris.attach(&probe).unwrap();
 
     let results = humility_vpd_lib::vpd_list(
         &hubris,
